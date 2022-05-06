bb3ae444104b4f77768244e1112fc455f32aee1d
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2022-05-05 21:16:40 -0300

    implements sending email and creating feedback

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "ee91009de036d7",
+    pass: "96518e1610130c"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Rodrigo Yuri <rodrigo33996@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
